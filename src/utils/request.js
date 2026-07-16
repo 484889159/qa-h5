d62e41d487ad47fc2f05c070a593f688f9865d95
484889159 <484889159@qq.com>
2026-07-17 02:11:36 +0800

    修改后端地址为Railway

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:8080'
+const baseURL = 'https://qa-backend-production-a82f.up.railway.app'
 
 const instance = axios.create({
   baseURL,

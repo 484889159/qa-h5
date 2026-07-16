d82f03eb08f16693714a713183214b43ad1d5c79
484889159 <484889159@qq.com>
2026-07-17 01:39:29 +0800

    修复request.js导出问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,57 @@
-const baseUrl = 'http://localhost:8080/api'
-export function request(url,data={},method='GET'){
-  return new Promise((resolve,reject)=>{
-    uni.request({ // 微信浏览器兼容fetch，也可直接用axios
-      url:baseUrl+url,
-      data,
-      method,
-      success:res=>resolve(res.data),
-      fail:err=>reject(err)
-    })
+import axios from 'axios'
+
+const baseURL = 'http://localhost:8080'
+
+const instance = axios.create({
+  baseURL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+instance.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 200) {
+      if (res.code === 401) {
+        localStorage.removeItem('token')
+        localStorage.removeItem('userInfo')
+        window.location.href = '/login'
+      }
+      return Promise.reject(new Error(res.msg || '请求失败'))
+    }
+    return res.data
+  },
+  error => {
+    console.error('请求错误:', error)
+    return Promise.reject(error)
+  }
+)
+
+// ✅ 导出所有方法
+export const request = (url, data = {}, method = 'GET') => {
+  return instance({
+    url,
+    method,
+    [method === 'GET' ? 'params' : 'data']: data
   })
 }
+
+export const get = (url, params) => request(url, params, 'GET')
+export const post = (url, data) => request(url, data, 'POST')
+export const put = (url, data) => request(url, data, 'PUT')
+export const del = (url, data) => request(url, data, 'DELETE')
+
+export default instance
